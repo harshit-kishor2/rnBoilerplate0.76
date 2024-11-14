210016f6b74ec045962f4210eb06b4f05fa8ca55
QL-Harshit-Kishor <harshit.kishor@quokkalabs.com>
2024-11-14 16:40:23 +0530

    Initialize project

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import {Text, View} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Welcome to React Native 0.76</Text>
+    </View>
+  );
+};
+
+export default App;
