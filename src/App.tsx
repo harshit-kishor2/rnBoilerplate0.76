e4c1d61ded13abe7c25cc49a2f1b3fcc1a69cf82
QL-Harshit-Kishor <harshit.kishor@quokkalabs.com>
2024-11-15 17:30:43 +0530

    [WIP]: eslint and lefthook setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View} from 'react-native';
+
+/**
+ * The root component of the application, which currently renders an empty view.
+ */
+const App = () => {
+  return (
+    <>
+      <View/>
+    </>
+  );
+};
+
+export default App;
