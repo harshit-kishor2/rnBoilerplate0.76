a0305e23ee3329600b5310ca7b33cdb6acabbcec
QL-Harshit-Kishor <harshit.kishor@quokkalabs.com>
2024-12-16 12:27:43 +0530

    [FIX]: some bugs

---
+++ src/helpers/enums.ts
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+
+export enum LoadingStatus {
+  IDLE = 'IDLE',
+  PENDING = 'PENDING',
+  FULLFILLED = 'FULLFILLED',
+  REJECTED = 'REJECTED'
+}
+
+export enum Gender {
+  MALE = 'MALE',
+  FEMALE = 'FEMALE',
+  OTHER = 'OTHER',
+}
--- src/helpers/strings.ts
+++ src/helpers/strings.ts
@@ -1,18 +1,3 @@
-/* eslint-disable no-unused-vars */
-
-export enum LoadingStatus {
-  IDLE = 'IDLE',
-  PENDING = 'PENDING',
-  FULLFILLED = 'FULLFILLED',
-  REJECTED = 'REJECTED'
-}
-
-export enum Gender {
-  MALE = 'MALE',
-  FEMALE = 'FEMALE',
-  OTHER = 'OTHER',
-}
-
 export const ApiConst = {
   twitterUrl: 'https://twitter.com',
   privacyPolicyUrl: 'https://docs.example.org/extras/legal/privacy-policy',
